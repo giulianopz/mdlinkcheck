a0b374317eb3b653ab3bff053861ef2d5256c2df
giuliano <panzironi.giuliano@gmail.com>
2023-09-06 22:56:30 +0200

    Fine-tune the stdlib http client as propossed here: https://gist.github.com/AngerM/5059b3ff16292f3fbee990bae7046381

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"flag"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"path"
 	"path/filepath"
 	"regexp"
+	"strings"
 	"time"
 
 	"golang.org/x/sync/errgroup"
+
+	"github.com/rs/dnscache"
 )
 
 var (
@@ -20,15 +25,20 @@ var (
 	skipTLS   bool
 	timeout   int64
 	userAgent string
+	method    string
 )
 
 var markdownRegex = regexp.MustCompile(`\[[^][]+]\((https?://[^()]+)\)`)
 
+const mdExt = ".md"
+
 var client = &http.Client{
 	Timeout: time.Duration(timeout) * time.Second,
+	// do not follow redirects
+	CheckRedirect: func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	},
 }
-
-const mdExt = ".md"
 
 func main() {
 
@@ -37,13 +47,55 @@ func main() {
 	flag.BoolVar(&skipTLS, "skip-tls", false, "ignore invalid TLS/SSL certificates (default false)")
 	flag.Int64Var(&timeout, "timeout", 5, "timeout in seconds")
 	flag.StringVar(&userAgent, "user-agent", "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0", "impersonate an agent")
+	flag.StringVar(&method, "http-method", http.MethodGet, "http verb: HEAD (faster, less accurate) or GET (slower, trustworthy)")
 	flag.Parse()
 
-	if skipTLS {
-		client.Transport = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		}
+	r := &dnscache.Resolver{}
+
+	t := &http.Transport{
+		// connection pooling params
+		MaxIdleConns:    100,
+		MaxConnsPerHost: 10,
+		IdleConnTimeout: 10 * time.Second,
+		// in-memory dns caching
+		DialContext: func(ctx context.Context, network string, addr string) (conn net.Conn, err error) {
+			host, port, err := net.SplitHostPort(addr)
+			if err != nil {
+				return nil, err
+			}
+			ips, err := r.LookupHost(ctx, host)
+			if err != nil {
+				return nil, err
+			}
+			for _, ip := range ips {
+				var dialer net.Dialer
+				conn, err = dialer.DialContext(ctx, network, net.JoinHostPort(ip, port))
+				if err == nil {
+					break
+				}
+			}
+			return
+		},
 	}
+
+	go func() {
+		t := time.NewTicker(5 * time.Minute)
+		defer t.Stop()
+
+		for range t.C {
+			options := dnscache.ResolverRefreshOptions{
+				ClearUnused:      true,
+				PersistOnFailure: true,
+			}
+			r.RefreshWithOptions(options)
+		}
+	}()
+
+	if skipTLS {
+		t.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	}
+
+	client.Transport = t
 
 	var e error
 	if file != "" {
@@ -71,6 +123,10 @@ func checkFile(file string) error {
 		return fmt.Errorf("not a markdown file")
 	}
 
+	if strings.ToUpper(method) != http.MethodHead && strings.ToUpper(method) != http.MethodGet {
+		return fmt.Errorf("http method must be HEAD or GET")
+	}
+
 	bs, err := os.ReadFile(file)
 	if err != nil {
 		return err
@@ -86,19 +142,20 @@ func checkFile(file string) error {
 
 		g.Go(func() error {
 
-			req, err := http.NewRequest(http.MethodGet, link, nil)
+			req, err := http.NewRequest(strings.ToUpper(method), link, nil)
 			if err != nil {
 				return err
 			}
 
 			req.Header.Add("User-Agent", userAgent)
+			req.Header.Add("Accept", "*/*")
 
 			resp, err := client.Do(req)
 			if err != nil {
 				fmt.Printf("[err]: %q\n", link)
 			} else {
-				resp.Body.Close()
 				fmt.Printf("[%d]: %s\n", resp.StatusCode, link)
+				defer resp.Body.Close()
 			}
 
 			return nil
